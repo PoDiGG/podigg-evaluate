7b4e485964187cf78ea25cf053e0f8b5495d5d21
Ruben Taelman <ruben.taelman@ugent.be>
2017-01-12 11:12:54 +0100

    Also measure memory usage

--- bin/performance.sh
+++ bin/performance.sh
@@ -2,6 +2,25 @@
 
 replications=5
 
+function measuremem {
+    TIMEFORMAT=%R
+    t=$(measuregenmem)
+    echo "scale=4;$t/$replications" | bc -l
+}
+
+function measuregenmem {
+    rm -rf output_data
+    tot="0"
+    for i in $(seq 1 $replications); do
+        ./memusg podigg-lc-env output_data
+        while [ ! -f mem.txt ]; do sleep 1; done
+        tot="$(cat mem.txt)+$tot"
+        rm mem.txt
+    done
+    rm -rf output_data
+    echo $tot | bc -l
+}
+
 function measure {
     TIMEFORMAT=%R
     t=$(time (for i in $(seq 1 $replications);do measuregen; done) 2>&1 1>/dev/null)
@@ -54,4 +73,31 @@ for connections in $allconnections; do
     export GTFS_GEN_ROUTES__ROUTES=$baseroutes
     export GTFS_GEN_CONNECTIONS__CONNECTIONS=$connections
     echo "$connections,$(measure)"
+done
+
+echo "--stops--"
+echo "amount,kb"
+for stops in $allstops; do
+    export GTFS_GEN_STOPS__STOPS=$stops
+    export GTFS_GEN_ROUTES__ROUTES=$baseroutes
+    export GTFS_GEN_CONNECTIONS__CONNECTIONS=$baseconnections
+    echo "$stops,$(measuremem)"
+done
+
+echo "--routes--"
+echo "amount,kb"
+for routes in $allroutes; do
+    export GTFS_GEN_STOPS__STOPS=$basestops
+    export GTFS_GEN_ROUTES__ROUTES=$routes
+    export GTFS_GEN_CONNECTIONS__CONNECTIONS=$baseconnections
+    echo "$routes,$(measuremem)"
+done
+
+echo "--connections--"
+echo "amount,kb"
+for connections in $allconnections; do
+    export GTFS_GEN_STOPS__STOPS=$basestops
+    export GTFS_GEN_ROUTES__ROUTES=$baseroutes
+    export GTFS_GEN_CONNECTIONS__CONNECTIONS=$connections
+    echo "$connections,$(measuremem)"
 done
