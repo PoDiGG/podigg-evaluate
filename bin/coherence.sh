baf234a6e10fd02684063a2a37dd926d7d660e11
Ruben Taelman <ruben.taelman@ugent.be>
2017-01-24 14:39:49 +0100

    Add datasets for coherence measurements

--- bin/coherence.sh
+++ bin/coherence.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-# Everything is generated between 20161201 and 20161331
-# graph-coherence nmbs-2016-12-1.ttl    0.984543934937036
+# graph-coherence nmbs-2016-12-1.ttl    0.984543934937036  (between 20161201 and 20161331)
 # 1: 0.9879878664074184
+# graph-coherence ns-2016-12-1.ttl    TODO  (between 20151201 and 20151331)
+# 1: 0.9870082003910289
+# graph-coherence delijn-2016-12-1.ttl    TODO  (between 20161201:00:00 and 20161201:05:20)
+# 1: 
 
 function generate {
     rm -rf output_data
@@ -12,9 +15,10 @@ function generate {
     echo "$1: $coverage"
 }
 
-echo "Real dataset: $(../node_modules/graph-coherence/bin/graph-coherence ../data/nmbs-2016-12-1.ttl)"
+#echo "Real train_BE: $(../node_modules/graph-coherence/bin/graph-coherence ../data/nmbs-2016-12-1.ttl)"
+echo "Real train_NL: $(../node_modules/graph-coherence/bin/graph-coherence ../data/ns-2015-12-1.ttl)"
+echo "Real bus_BE: $(../node_modules/graph-coherence/bin/graph-coherence ../data/delijn-2016-12-1.ttl)"
 
-generate 1
-#generate 2
-#generate 3
-#generate 4
+#generate train_BE
+#generate train_NL
+generate bus_BE
